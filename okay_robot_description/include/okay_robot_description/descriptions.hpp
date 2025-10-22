878f5bbf023bbad9f58f652b322b5c567ab3c314
eeerogers <eric.rogers621@gmail.com>
2025-10-22 19:12:20 -0400

    update urdf stuff

--- okay_robot_description/include/okay_robot_description/descriptions.hpp
+++ okay_robot_description/include/okay_robot_description/descriptions.hpp
@@ -10,38 +10,8 @@ inline const std::string& get_package_dir()
     return package_dir;
 }
 
-inline const std::string& get_test_xml_path()
-{
-    static const std::string path = get_package_dir() + "/scene.xml";
-    return path;
-}
-
 inline const std::string& get_okay_robot_xml_path()
 {
     static const std::string path = get_package_dir() + "/okay_robot/scene.xml";
-    return path;
-}
-
-inline const std::string& get_jaco3_xml_path()
-{
-    static const std::string path = get_package_dir() + "/kinova_gen3/scene.xml";
-    return path;
-}
-
-inline const std::string& get_lerobot_xml_path()
-{
-    static const std::string path = get_package_dir() + "/low_cost_robot_arm/scene.xml";
-    return path;
-}
-
-inline const std::string& get_tidybot_xml_path()
-{
-    static const std::string path = get_package_dir() + "/stanford_tidybot/scene.xml";
-    return path;
-}
-
-inline const std::string& get_unitree_g1_xml_path()
-{
-    static const std::string path = get_package_dir() + "/unitree_g1/scene.xml";
     return path;
 }

8736728713035e2bfad3ae8c62da4e8a326ec31f
eeerogers <eric.rogers621@gmail.com>
2025-11-11 22:10:25 -0500

    fill out the rest of the servo feedback data in published message

--- okay_robot_driver/src/servo_bus/servo_bus_node.cpp
+++ okay_robot_driver/src/servo_bus/servo_bus_node.cpp
@@ -37,10 +37,29 @@ void ServoBusNode::publish_observation()
 
     std::vector<uint8_t> data_buffer;
     int servo_position;
-    float angle_position;
+    int servo_speed;
+    int servo_load;
+    int servo_voltage;
+    int servo_current;
 
+    float servo_position_rad;
+    float servo_speed_rad_s;
+    float servo_load_pct;
+    float servo_voltage_v;
+    float servo_current_a;
+
+    // position: 2 bytes
+    // speed: 2 bytes
+    // load: 2 bytes
+    // operating voltage: 1 byte
+    // temperature: 1 byte
+    // async write flag: 1 byte
+    // servo status: 1 byte
+    // move flag: 1 byte
+    // also position?: 2 bytes
+    // current: 2 bytes
     std::vector<okay_robot_msgs::msg::ServoObservation> observations;
-    std::vector<uint8_t> data({ ServoRegister::CURRENT_POSITION, 0x02 });
+    std::vector<uint8_t> data({ ServoRegister::CURRENT_POSITION, 0x0F });
 
     for (uint8_t i = 1; i <= 7; i++) {
         data_buffer = this->servo_bus_.read_data(i, data);
@@ -52,21 +71,39 @@ void ServoBusNode::publish_observation()
 
         // need to flip the position because the servo outputs reversed direction
         servo_position = 4095 - ((data_buffer[6] << 8) + data_buffer[5]);
-        angle_position = servo_position * (1.0 / this->rad_to_range_);
+        servo_speed = ((data_buffer[8] << 8) | data_buffer[7]);
+        servo_load = ((data_buffer[10] << 8) | data_buffer[9]);
+        servo_voltage = data_buffer[11];
+        servo_current = ((data_buffer[19] << 8) | data_buffer[18]);
 
+        // convert signed values
+        servo_position
+            = servo_position & (1 << 15) ? -(servo_position & ~(1 << 15)) : servo_position;
+        servo_speed = servo_speed & (1 << 15) ? -(servo_speed & ~(1 << 15)) : servo_speed;
+        servo_load = servo_load & (1 << 10) ? -(servo_load & ~(1 << 10)) : servo_load;
+        servo_current = servo_current & (1 << 15) ? -(servo_current & (1 << 15)) : servo_current;
+
+        // map to units
+        servo_position_rad = servo_position * (1.0 / this->rad_to_range_);
+        servo_speed_rad_s = servo_speed * (1.0 / this->rad_to_range_);
+        servo_load_pct = servo_load * 0.1;
+        servo_voltage_v = servo_voltage * 0.1;
+        servo_current_a = servo_current * 0.0065;
+
+        // build ros message
         auto new_observation = okay_robot_msgs::msg::ServoObservation();
         new_observation.id = data_buffer[2];
-        new_observation.position = angle_position;
+        new_observation.position = servo_position_rad;
+        new_observation.speed = servo_speed_rad_s;
+        new_observation.load = servo_load_pct;
+        new_observation.voltage = servo_voltage_v;
+        new_observation.temperature = data_buffer[12];
+        new_observation.current = servo_current_a;
+        new_observation.is_moving = data_buffer[15];
         new_observation.status = data_buffer[4];
-
-        printf("i: %i, id: %i\n", i, new_observation.id);
-        if (i != new_observation.id) {
-            print_message(data_buffer);
-        }
 
         observations.push_back(new_observation);
     }
-    printf("------------------------------\n");
 
     auto servo_bus_observation = okay_robot_msgs::msg::ServoBusObservation();
     servo_bus_observation.observations = observations;

e07325fbf4b640d3ca9afee5bcc000f1cf75af1b
eeerogers <eric.rogers621@gmail.com>
2025-12-22 06:45:59 -0500

    add tabs to switch plots

--- okay_robot_view/src/view_node.cpp
+++ okay_robot_view/src/view_node.cpp
@@ -15,6 +15,41 @@ void glfw_error_callback_(int error, const char* description)
     std::cerr << "GLFW error " << error << ": " << description << std::endl;
 }
 
+void line_plot(
+    CircularBuffer<okay_robot_msgs::msg::ServoBusObservation::SharedPtr>& buffer, std::mutex& mutex)
+{
+    // try to plot joint positions
+    if (ImPlot::BeginPlot("Joint Positions", ImVec2(-1, -1))) {
+        std::lock_guard<std::mutex> lock(mutex);
+
+        std::vector<float> times;
+        std::unordered_map<int, std::vector<float>> positions;
+        for (const auto& bus_obs : buffer) {
+            // just skip if the full joint space is not represented for now
+            if (bus_obs->observations.size() != 7)
+                continue;
+
+            times.push_back(bus_obs->observations[0].time);
+            for (const auto& obs : bus_obs->observations) {
+                if (positions.find(obs.id) != positions.end()) {
+                    positions[obs.id].push_back(obs.position);
+                } else {
+                    positions[obs.id] = std::vector<float>({ obs.position });
+                }
+            }
+        }
+
+        ImPlot::SetupAxes("time", "position (rad)");
+        if (times.size() > 0)
+            ImPlot::SetupAxesLimits(times.front(), times.back(), 0.0f, 2 * M_PIf, ImGuiCond_Always);
+        for (auto& plot : positions) {
+            ImPlot::PlotLine(("j" + std::to_string(plot.first)).c_str(), times.data(),
+                plot.second.data(), times.size());
+        }
+        ImPlot::EndPlot();
+    }
+}
+
 void spin_window(CircularBuffer<okay_robot_msgs::msg::ServoBusObservation::SharedPtr>& buffer,
     std::atomic<bool>& shutdown_flag, std::mutex& mutex)
 {
@@ -45,7 +80,7 @@ void spin_window(CircularBuffer<okay_robot_msgs::msg::ServoBusObservation::Share
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize
+    static ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize
         | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar;
 
     while (!(glfwWindowShouldClose(window) || shutdown_flag.load())) {
@@ -57,7 +92,7 @@ void spin_window(CircularBuffer<okay_robot_msgs::msg::ServoBusObservation::Share
         ImGui::NewFrame();
 
         // make the plot fill up the window
-        int display_w, display_h;
+        static int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
 
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
@@ -66,36 +101,13 @@ void spin_window(CircularBuffer<okay_robot_msgs::msg::ServoBusObservation::Share
 
         ImGui::Begin("okay_robot", nullptr, window_flags);
 
-        // try to plot joint positions
-        if (ImPlot::BeginPlot("Joint Positions", ImVec2(-1, -1))) {
-            std::lock_guard<std::mutex> lock(mutex);
-
-            std::vector<float> times;
-            std::unordered_map<int, std::vector<float>> positions;
-            for (const auto& bus_obs : buffer) {
-                // just skip if the full joint space is not represented for now
-                if (bus_obs->observations.size() != 7)
-                    continue;
-
-                times.push_back(bus_obs->observations[0].time);
-                for (const auto& obs : bus_obs->observations) {
-                    if (positions.find(obs.id) != positions.end()) {
-                        positions[obs.id].push_back(obs.position);
-                    } else {
-                        positions[obs.id] = std::vector<float>({ obs.position });
-                    }
-                }
+        if (ImGui::BeginTabBar("ImPlotDemoTabs")) {
+            if (ImGui::BeginTabItem("Positions")) {
+                line_plot(buffer, mutex);
+                ImGui::EndTabItem();
             }
-
-            ImPlot::SetupAxes("time", "position (rad)");
-            ImPlot::SetupAxesLimits(times.front(), times.back(), 0.0f, 2 * M_PIf, ImGuiCond_Always);
-            for (auto& plot : positions) {
-                ImPlot::PlotLine(("j" + std::to_string(plot.first)).c_str(), times.data(),
-                    plot.second.data(), times.size());
-            }
-            ImPlot::EndPlot();
+            ImGui::EndTabBar();
         }
-
         ImGui::End();
         ImGui::PopStyleVar();
 
